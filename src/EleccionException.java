36aa63bc097aaf9856d663379ab8ef954e151d2a
chungyentsai <39892208+chungyentsai@users.noreply.github.com>
2018-06-05 00:36:44 -0300

    Update EleccionException.java

--- src/EleccionException.java
+++ src/EleccionException.java
@@ -1,11 +1,5 @@
 public class EleccionException extends Exception {
     
-
-    /**
-     * 
-     */
-    private static final long serialVersionUID = 1L;
-
     public EleccionException (String msg){
         super(msg);
     }

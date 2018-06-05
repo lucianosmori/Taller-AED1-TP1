2623be7de1e4e1a04453223509591b08b29bf3bf
chungyentsai <39892208+chungyentsai@users.noreply.github.com>
2018-06-05 19:07:59 -0300

    Update Vehiculo.java

--- src/Vehiculo.java
+++ src/Vehiculo.java
@@ -1,15 +1,31 @@
 public abstract class Vehiculo {
-    private String vin;
-    public String getVin() {
-        return vin;
-    }
-    public void setVin(String vin) {
-        this.vin = vin;
-    }
-     public String toString(){
-            return vin;
-        }
-    public Vehiculo(String vin){
-        this.vin = vin;
-    }    
+	private String vin;
+	private float motorLitros;
+	private int motorCilindradas;
+	
+	public String getVin() {
+		return vin;
+	}
+	public void setVin(String vin) {
+		this.vin = vin;
+	}
+	
+	public float getMotorLitros() {
+		return motorLitros;
+	}
+	public void setMotorLitros(float motorLitros) {
+		this.motorLitros = motorLitros;
+	}
+	public int getMotorCilindradas() {
+		return motorCilindradas;
+	}
+	public void setMotorCilindradas(int motorCilindradas) {
+		this.motorCilindradas = motorCilindradas;
+	}
+	
+	public Vehiculo(String vin, float motorLitros, int motorCilindradas){
+		this.vin = vin;
+		this.motorLitros=motorLitros;
+		this.motorCilindradas=motorCilindradas;
+	}
 }

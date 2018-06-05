7ecfc688822c91d3f548b9575d3b251e6e0c3796
chungyentsai <39892208+chungyentsai@users.noreply.github.com>
2018-06-05 19:07:41 -0300

    Update Auto.java

--- src/Auto.java
+++ src/Auto.java
@@ -1,16 +1,18 @@
 public class Auto extends Vehiculo{
-    private String airbags;
-     public String toString(){
-            return super.toString();
-        }    
-     public Auto (String vin, String contenido){
-         super(vin);
-         this.setContenido(contenido);
-     }
-    public String getContenido() {
-        return airbags;
-    }
-    public void setContenido(String contenido) {
-        this.airbags = contenido;
-    }
+	private String airbags;
+	
+	 public String toString(){
+	        return super.toString();
+	    }	
+	 public Auto (String vin, float motorLitros, int motorCilindradas, String contenido){
+		 super(vin,motorLitros,motorCilindradas);
+	     this.setContenido(contenido);
+	 }
+	
+	public String getContenido() {
+		return airbags;
+	}
+	public void setContenido(String contenido) {
+		this.airbags = contenido;
+	}
 }
